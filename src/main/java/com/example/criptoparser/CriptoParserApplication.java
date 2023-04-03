cedcd7db63d6a7e08eca1819cd5b6401fbe032e9
Misha Brovkin <haasiakira@gmail.com>
2023-04-03 12:15:08 +0300

    Initial commit

---
+++ src/main/java/com/example/criptoparser/CriptoParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.criptoparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriptoParserApplication.class, args);
+    }
+
+}
